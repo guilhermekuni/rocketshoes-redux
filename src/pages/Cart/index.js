6bc3ce6f11d9896200fb574f4f6c20c6d1373b3c
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-01 17:16:22 -0300

    implements remove action

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -4,7 +4,7 @@ import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -44,7 +44,7 @@ function Cart({ cart }) {
                 </td>
                 <td>
                   <div>
-                    <button type="button">
+                    <button type="button" onClick={() => dispatch({ type: 'REMOVE_FROM_CART', id: product.id })}>
                       <MdDelete size={20} color="#7159c1" />
                     </button>
                   </div>
