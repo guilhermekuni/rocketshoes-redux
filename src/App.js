88391f692c324c65ffe62ed69626eee1db94b64e
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-01 12:16:50 -0300

    configure reactotron and eslint

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
-import Header from '../src/components/Header';
+import Header from './components/Header';
 import Routes from './routes';
 
 import store from './store';
