74674a064804b85a959f9e9b3d8dee1d34fe6a55
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-01 21:20:22 -0300

    refactor actions, reducers and redux saga middlewares

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/roottSaga';
+import rootSaga from './modules/rootSaga';
 
 const sagaMonitor = process.env.NODE_ENV === 'development'
   ? console.tron.createSagaMonitor()
