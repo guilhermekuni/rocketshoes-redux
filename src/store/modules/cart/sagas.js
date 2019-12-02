62d742aff1f97d4e62f04dd87dbc4e1dc1024fea
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2019-12-01 21:31:24 -0300

    implements navigation that awaits redux saga methods execute

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,6 +4,7 @@ import {
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../utils/format';
 
 import { addToCartSuccess, updateAmountSuccess } from './actions';
@@ -35,6 +36,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
